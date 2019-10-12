36d44b1684e46526ed123ebad837623e051b9285
Mikhail Rybinski <pootsar@gmail.com>
2019-10-12 23:16:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let strBr = str.split('');
+  console.log(str);
+  for(let i =0; i < strBr.length; i++){
+    for( let j= 0; j< bracketsConfig.length; j++){
+      let first = bracketsConfig[j][0];
+     // console.log("first = ", first);
+      let second = bracketsConfig[j][1];
+     // console.log("second = ", second);
+      if(strBr[i] == first && strBr[i+1]== second){
+        strBr.splice(i,2);
+        i-=2;
+      }
+    }  
+  }
+  if (strBr.length == 0)return true;
+  else return false;
 }
